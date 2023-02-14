79b5e0430d94f14d078b31578a6cd808f0b51c14
fergy-custom <fergyfive55@gmail.com>
2023-02-13 19:08:56 -0500

    C answer for license question

--- LeetCode/Easy/482_licenseKeyFormatting.c
+++ LeetCode/Easy/482_licenseKeyFormatting.c
@@ -1,39 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 char * licenseKeyFormatting(char * s, int k){
 
-    int j = 0;
+    int originalLen = 0;
 
-    char * tmpS = (char *)malloc( sizeof( s ) );
-
-    for( int i = 0; s[i] != NULL; i++ )
+    for( int i = 0; s[i] != '\0'; i++ )
     {
-        printf("s[%d] = %c", i, s[i] );
+        ( s[i] != '-' ) ? originalLen++ : originalLen;
+    }
 
-        printf(s[i] == '-' ? " True\n" : " False\n" );
+    char * tmpS = (char *)calloc( originalLen, sizeof( char ) );
 
-        if( s[i] != '-' )
+    for( int idx = 0, i = 0; s[idx] != '\0'; idx++ )
+    {
+        if( s[idx] != '-' )
         {
-            tmpS[j] = s[i];
-            j++;
+            tmpS[i] = s[idx];
+            i++;
         }
     }
 
-    // free( s );
-    char * s = tmpS;
-    // free( tmpS );
+    s = (char *)malloc( sizeof( tmpS ) );
+    memcpy( &s, &tmpS, sizeof( tmpS ) );
 
-    int len = j - 1;
+    int numberOfDashes = ( originalLen % k == 0 ) ? (originalLen / k) - 1 : originalLen / k;
+    tmpS = (char *)calloc( originalLen + numberOfDashes, sizeof( char ) );
+    
+    int len = originalLen + numberOfDashes;
 
-    for( len; len >= 0; len-- )
+    for( int j = len-1, i = originalLen-1; j >= 0; i--, j-- )
     {
-        len % k != 0 ? printf("%c", s[len] ) : len == 0 ? printf("%c", s[len] ) : printf("%c-", s[len] );
+        if( ( (originalLen-(i)) % k != 0 ) || ( j == 0 ) )
+        {
+            tmpS[ j ] = s[ i ];
+        }
+        else
+        {
+            tmpS[ j ] = s[ i ];
+            j--;
+            tmpS[ j ] = '-';
+        }
+
     }
+    return tmpS;
 }
 
 int main()
 {
-    licenseKeyFormatting( "5F3Z-2e-9-w", 4 );
+    char * answer = licenseKeyFormatting( "5F3Z-2e-9-w", 4 );
+    for( int i = 0; answer[i] != '\0' ; i++ ){printf("%c", answer[i] );} printf("\n");
+
+    char * answer2 = licenseKeyFormatting( "2-5g-3-J", 2 );
+    for( int i = 0; answer2[i] != '\0' ; i++ ){printf("%c", answer2[i] );} printf("\n");
 
     return 0;
 }
