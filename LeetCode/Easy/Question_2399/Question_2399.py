107652d03cd5dc6f2e418675b6f85b4ce1dcb95d
Eli-Ferguson-Pro-X <elijuddferguson@gmail.com>
2023-04-23 14:34:13 -0400

    comments

--- LeetCode/Easy/Question_2399/Question_2399.py
+++ LeetCode/Easy/Question_2399/Question_2399.py
@@ -1,14 +1,19 @@
 class Solution:
     def checkDistances(self, s: str, distance: list[int]) -> bool:
-
+        
+        #  arr of length letters
         dist = [ -1 ] * 26
 
+        # for each letter in s check if it is in dist arr
+        # if it is check to see if the dist matches the given
+        # else add the current index to dist at that letter
         idx = 0
         for i in range( len( s ) ) :
 
             idx = ord( s[ i ] ) - 97
 
             if dist[ idx ] != -1 :
+                # if distances don't match return False
                 if i - dist[ idx ] - 1 != distance[ idx ] :
                     return False
             else :
