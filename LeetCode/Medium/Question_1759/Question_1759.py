62132efa0c53ab3c54fe963446024e6f8e203063
Eli-Ferguson-Pro-X <elijuddferguson@gmail.com>
2023-04-25 16:07:17 -0400

    Easy and Medium answered

---
+++ LeetCode/Medium/Question_1759/Question_1759.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countHomogenous(self, s: str) -> int:
+
+        count = 0
+        start = 0
+
+        while start < len( s ):
+
+            i = 1
+
+            while start+i < len( s ) :
+                if s[ start ] == s[ start + i ] : i+=1
+                else : break
+
+            start += i
+        
+            while i :
+                count += i 
+                i -= 1
+
+        return count % ( ( 10 ** 9) + 7 )
+    
+s = "abbcccaa"
+
+print( f'Homogeneous Sub-Strings Count: {Solution.countHomogenous( super, s )}' )    
+
+# Beats 11.68% Runtime, 275ms
+# Beats 77.27% Memory, 14.8mb
